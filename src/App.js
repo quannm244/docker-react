d4fc6c36866346b962dadd0a7cd19e9aa85021d7
Quan Nguyen <quannm244@gmail.com>
2020-03-06 14:29:03 +0700

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ly oi anh yeu em
+          Muk!
         </p>
         <a
           className="App-link"
